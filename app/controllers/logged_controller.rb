7e4b93fee79522347b0648dec13a7019d97c5cd4
pivarci <pivarci.matej@gmail.com>
2016-04-19 22:10:56 +0200

    smart listing gem

--- app/controllers/logged_controller.rb
+++ app/controllers/logged_controller.rb
@@ -1,5 +1,9 @@
 class LoggedController < ApplicationController
+  include SmartListing::Helper::ControllerExtensions
+  helper  SmartListing::Helper
+
   before_action :authenticate_user!
+
   layout "logged"
   prepend_view_path "app/views/logged"
 end
