9217771665b54892935484b242a2168ebf567793
pivarci <pivarci.matej@gmail.com>
2016-04-20 02:40:30 +0200

    users clean

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :categories
-  has_many :accounts
+  has_many :categories, dependent: :destroy
+  has_many :accounts, dependent: :destroy
 end
