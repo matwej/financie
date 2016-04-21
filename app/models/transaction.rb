9975309c5f21e5f85411f2e0382551c620dede00
pivarci <pivarci.matej@gmail.com>
2016-04-21 03:59:10 +0200

    mail notification on warning amount on account vol.2

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -20,20 +20,20 @@ class Transaction < ActiveRecord::Base
 
   def save_and_notify
     res = self.save
-    notify self.owner if res
+    notify owner if res
     res
   end
 
   def update_and_notify(params)
     res = self.update params
-    notify self.owner if res
+    notify owner if res
     res
   end
 
   private
 
   def notify(account)
-    if account.notified && account.warning_limit <= account.sum_of_transactions
+    if account.notified && account.warning_limit >= account.sum_of_transactions
       UserMailer.warning_limit_email(account.user, account).deliver_now
     end
   end
